3b7b387b2400e2b502974dd68b8781e3ddcdb9a5
RobertMachi <robert.mc@hotmail.es>
2019-04-11 17:44:39 +0200

    Changed the request from the API to a local data.json so we don´t waste the 50 requests avaliables

--- src/App.js
+++ src/App.js
@@ -6,33 +6,35 @@ import RecipeDetails from "./components/RecipeDetails";
 
 class App extends Component {
   state = {
-    recipes:[],
+    recipes: recipes,
     url:"https://www.food2fork.com/api/search?key=80c5575dc7188084b7e44abe520a3ed8"
   };
-
-  async getRecipes(){
-    try {
-      const data = await fetch(this.state.url);
-      const jsonData = await data.json();
-      this.setState({
-        recipes: jsonData.recipes
-      })
-    }catch (error) {
-      console.log(error);
-    }
-  }
-
-  //after app component mount then we can use the method
-  componentDidMount() {
-    this.getRecipes()
-  }
+  //-------------------------------------------------------------------------------
+  //this is for the API, now I´ll work with a personal data
+  // async getRecipes(){
+  //   try {
+  //     const data = await fetch(this.state.url);
+  //     const jsonData = await data.json();
+  //     this.setState({
+  //       recipes: jsonData.recipes
+  //     })
+  //   }catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+  //
+  // //after app component mount then we can use the method
+  // componentDidMount() {
+  //   this.getRecipes()
+  // }
+  //---------------------------------------------------------------
 
   render() {
-    console.log(this.state.recipes);
+    //console.log(this.state.recipes);
 
     return (
       <React.Fragment>
-        <RecipeList/>
+        <RecipeList recipes={this.state.recipes} />
         <RecipeDetails/>
       </React.Fragment>
     );
